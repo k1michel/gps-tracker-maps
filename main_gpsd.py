d33171c45294d28fab5c7906af10e34c756ac919
Miguel Angel Alvarez Clemente <michel_civic@hotmail.com>
2022-10-05 22:32:28 +0100

    Obtiene coordenadas y mas datos del GPS, muestra todo por terminal y abre URL en navegador con mapa. Descartados GoogleMaps y TOMTOM porque no sirven para mi proposito. Siguiente codigo para GUI visualizando la URL del mapa permanentemente.

--- main_gpsd.py
+++ main_gpsd.py
@@ -1,75 +1,82 @@
-#!/usr/bin/env python3
-
 import gpsd
-import googlemaps
 from datetime import datetime
+import requests
+import json
+import webbrowser
 
-# Connect to the local gpsd
+#Conectar con localhost de gpsd
 gpsd.connect(host='localhost')
 
-# Connect somewhere else
-#gpsd.connect()
 
-# Get gps position
+#Obtener datos del gps
 packet = gpsd.get_current()
 
-# See the inline docs for GpsResponse for the available data
-print(" ************ PROPERTIES ************* ")
-print("  Mode: " + str(packet.mode))
-print("Satellites: " + str(packet.sats))
+#Datos de respuesta del gps
+print(" ************ DATOS DEL GPS ************* ")
+print("  MODO: " + str(packet.mode))
+print("SATELITES: " + str(packet.sats))
 if packet.mode >= 2:
-    print("  Latitude: " + str(packet.lat))
-    print(" Longitude: " + str(packet.lon))
-    print(" Track: " + str(packet.track))
-    print("  Horizontal Speed: " + str(packet.hspeed))
-    print(" Time: " + str(packet.time))
-    print(" Error: " + str(packet.error))
+    print("  LATITUD: " + str(packet.lat))
+    print(" LONGITUD: " + str(packet.lon))
+    print(" TRACK: " + str(packet.track))
+    print("  VELOCIDAD HORIZONTAL: " + str(packet.hspeed))
+    print(" FECHA: " + str(packet.time))
+    print(" ERROR: " + str(packet.error))
 else:
-    print("  Latitude: NOT AVAILABLE")
-    print(" Longitude: NOT AVAILABLE")
-    print(" Track: NOT AVAILABLE")
-    print("  Horizontal Speed: NOT AVAILABLE")
-    print(" Error: NOT AVAILABLE")
+    print("  LATITUD: SIN DATOS")
+    print(" LONGITUD: SIN DATOS")
+    print(" TRACK: SIN DATOS")
+    print("  VELOCIDAD HORIZONTAL: SIN DATOS")
+    print(" ERROR: SIN DATOS")
 
 if packet.mode >= 3:
-    print("  Altitude: " + str(packet.alt))
-    print(" Climb: " + str(packet.climb))
+    print("  ALTITUD: " + str(packet.alt))
+    print(" ELEVACION: " + str(packet.climb))
 else:
-    print("  Altitude: NOT AVAILABLE")
-    print(" Climb: NOT AVAILABLE")
+    print("  ALTITUD: SIN DATOS")
+    print(" ELEVACION: SIN DATOS")
 
-print(" ************** METHODS ************** ")
+print(" ************** DATOS COMPLEJOS DEL GPS ************** ")
 if packet.mode >= 2:
-    print("  Location: " + str(packet.position()))
-    print(" Speed: " + str(packet.speed()))
-    print("Position Precision: " + str(packet.position_precision()))
-    #print("  Time UTC: " + str(packet.time_utc()))
-    #print("Time Local: " + str(packet.time_local()))
-    print("   Map URL: " + str(packet.map_url()))
+    print("  LOCALIZACION: " + str(packet.position()))
+    print(" VELOCIDAD: " + str(packet.speed()))
+    print("PRECISION DE POSICION: " + str(packet.position_precision()))
+    print("   MAPA URL: " + str(packet.map_url()))
 else:
-    print("  Location: NOT AVAILABLE")
-    print(" Speed: NOT AVAILABLE")
-    print("Position Precision: NOT AVAILABLE")
-    print("  Time UTC: NOT AVAILABLE")
-    print("Time Local: NOT AVAILABLE")
-    print("   Map URL: NOT AVAILABLE")
+    print("  LOCALIZACION: SIN DATOS")
+    print(" VELOCIDAD: SIN DATOS")
+    print(" PRECISION DE POSICION: SIN DATOS")
+    print("   MAPA URL: SIN DATOS")
 
 if packet.mode >= 3:
-    print("  Altitude: " + str(packet.altitude()))
-    # print("  Movement: " + str(packet.movement()))
-    # print("  Speed Vertical: " + str(packet.speed_vertical()))
+    print("  ALTITUD: " + str(packet.altitude()))
 else:
-    print("  Altitude: NOT AVAILABLE")
-    # print("  Movement: NOT AVAILABLE")
-    # print(" Speed Vertical: NOT AVAILABLE")
+    print("  ALTITUD: SIN DATOS")
 
-print(" ************* FUNCTIONS ************* ")
-print("Device: " + str(gpsd.device()))
 
-print('\nMIS DATOS\n')
+print(" ************* FUNCIONES DEL GPS ************* ")
+print("DISPOSITIVO: " + str(gpsd.device()))
+
+print('\n ##########  MIS DATOS  #########')
 lat,lon = packet.position()
 print(f'Latitud: {lat} \nLongitud: {lon}')
 
+url_map = str(packet.map_url())
+webbrowser.open(url_map)
+
+
+
+### TOM TOM ###
+'''
+#locations = str(round(lat,5)) + ',' + str(round(lon,5)) + ':' + str(round(lat+10,5)) + ',' + str(round(lon+10,5))
+#post_tomtom=requests.get(f'https://api.tomtom.com/routing/1/calculateRoute/{locations}/json?key=0RIbAqPHzVq7IyARS5U9Uig03lOGtRoS')
+post_tomtom=requests.get(f'https://api.tomtom.com/map/1/staticimage?key=0RIbAqPHzVq7IyARS5U9Uig03lOGtRoS&zoom=15&center={lon},{lat}&format=png&layer=basic&style=main&width=300&height=200&view=Unified&language=es-ES').content
+with open(f'mapa_{packet.time}.png','wb') as down_img:
+    down_img.write(post_tomtom)
+'''
+
+### GOOGLE MAPS ###
+'''
 gmaps = googlemaps.Client(key='-')
 
 # Geocoding an address
@@ -83,3 +90,4 @@ reverse_geocode_result = gmaps.reverse_geocode((lat, lon))
 # Request directions via public transit
 now = datetime.now()
 directions_result = gmaps.directions("GPS", "Lugar, Pais", mode="transit", departure_time=now)
+'''
