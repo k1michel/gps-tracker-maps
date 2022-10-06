c33b5de78380c4dd44782ba85da32f99944555c0
Miguel Angel Alvarez Clemente <michel_civic@hotmail.com>
2022-10-06 20:18:12 +0100

    Creacion de API REST para poder recepcionar los datos del gps. Funcionando OK.

--- main_gpsd.py
+++ main_gpsd.py
@@ -3,66 +3,84 @@ from datetime import datetime
 import requests
 import json
 import webbrowser
+from typing import Optional   
+from fastapi import FastAPI, Request
+from pydantic import BaseModel  
+import uvicorn
 
-#Conectar con localhost de gpsd
-gpsd.connect(host='localhost')
-
-
-#Obtener datos del gps
-packet = gpsd.get_current()
-
-#Datos de respuesta del gps
-print(" ************ DATOS DEL GPS ************* ")
-print("  MODO: " + str(packet.mode))
-print("SATELITES: " + str(packet.sats))
-if packet.mode >= 2:
-    print("  LATITUD: " + str(packet.lat))
-    print(" LONGITUD: " + str(packet.lon))
-    print(" TRACK: " + str(packet.track))
-    print("  VELOCIDAD HORIZONTAL: " + str(packet.hspeed))
-    print(" FECHA: " + str(packet.time))
-    print(" ERROR: " + str(packet.error))
-else:
-    print("  LATITUD: SIN DATOS")
-    print(" LONGITUD: SIN DATOS")
-    print(" TRACK: SIN DATOS")
-    print("  VELOCIDAD HORIZONTAL: SIN DATOS")
-    print(" ERROR: SIN DATOS")
-
-if packet.mode >= 3:
-    print("  ALTITUD: " + str(packet.alt))
-    print(" ELEVACION: " + str(packet.climb))
-else:
-    print("  ALTITUD: SIN DATOS")
-    print(" ELEVACION: SIN DATOS")
-
-print(" ************** DATOS COMPLEJOS DEL GPS ************** ")
-if packet.mode >= 2:
-    print("  LOCALIZACION: " + str(packet.position()))
-    print(" VELOCIDAD: " + str(packet.speed()))
-    print("PRECISION DE POSICION: " + str(packet.position_precision()))
-    print("   MAPA URL: " + str(packet.map_url()))
-else:
-    print("  LOCALIZACION: SIN DATOS")
-    print(" VELOCIDAD: SIN DATOS")
-    print(" PRECISION DE POSICION: SIN DATOS")
-    print("   MAPA URL: SIN DATOS")
-
-if packet.mode >= 3:
-    print("  ALTITUD: " + str(packet.altitude()))
-else:
-    print("  ALTITUD: SIN DATOS")
+## CREACION API ##
+api_gpsd = FastAPI() 
 
 
-print(" ************* FUNCIONES DEL GPS ************* ")
-print("DISPOSITIVO: " + str(gpsd.device()))
+## PETICIONES ##
+@api_gpsd.get("/url")
+def get_envios():  
+    #Conectar con localhost de gpsd
+    gpsd.connect(host='localhost')
 
-print('\n ##########  MIS DATOS  #########')
-lat,lon = packet.position()
-print(f'Latitud: {lat} \nLongitud: {lon}')
 
-url_map = str(packet.map_url())
-webbrowser.open(url_map)
+    #Obtener datos del gps
+    packet = gpsd.get_current()
+
+    #Datos de respuesta del gps
+    print(" ************ DATOS DEL GPS ************* ")
+    print("  MODO: " + str(packet.mode))
+    print("SATELITES: " + str(packet.sats))
+    if packet.mode >= 2:
+        print("  LATITUD: " + str(packet.lat))
+        print(" LONGITUD: " + str(packet.lon))
+        print(" TRACK: " + str(packet.track))
+        print("  VELOCIDAD HORIZONTAL: " + str(packet.hspeed))
+        print(" FECHA: " + str(packet.time))
+        print(" ERROR: " + str(packet.error))
+    else:
+        print("  LATITUD: SIN DATOS")
+        print(" LONGITUD: SIN DATOS")
+        print(" TRACK: SIN DATOS")
+        print("  VELOCIDAD HORIZONTAL: SIN DATOS")
+        print(" ERROR: SIN DATOS")
+
+    if packet.mode >= 3:
+        print("  ALTITUD: " + str(packet.alt))
+        print(" ELEVACION: " + str(packet.climb))
+    else:
+        print("  ALTITUD: SIN DATOS")
+        print(" ELEVACION: SIN DATOS")
+
+    print(" ************** DATOS COMPLEJOS DEL GPS ************** ")
+    if packet.mode >= 2:
+        print("  LOCALIZACION: " + str(packet.position()))
+        print(" VELOCIDAD: " + str(packet.speed()))
+        print("PRECISION DE POSICION: " + str(packet.position_precision()))
+        print("   MAPA URL: " + str(packet.map_url()))
+    else:
+        print("  LOCALIZACION: SIN DATOS")
+        print(" VELOCIDAD: SIN DATOS")
+        print(" PRECISION DE POSICION: SIN DATOS")
+        print("   MAPA URL: SIN DATOS")
+
+    if packet.mode >= 3:
+        print("  ALTITUD: " + str(packet.altitude()))
+    else:
+        print("  ALTITUD: SIN DATOS")
+
+
+    print(" ************* FUNCIONES DEL GPS ************* ")
+    print("DISPOSITIVO: " + str(gpsd.device()))
+
+    print('\n ##########  MIS DATOS  #########')
+    lat,lon = packet.position()
+    print(f'Latitud: {lat} \nLongitud: {lon}')
+
+    url_map = str(packet.map_url())
+    envio= dict(
+        latitud = lat,
+        longitud = lon,
+        url = url_map,
+
+    )
+    return envio
+#webbrowser.open(url_map)
 
 
 
@@ -91,3 +109,6 @@ reverse_geocode_result = gmaps.reverse_geocode((lat, lon))
 now = datetime.now()
 directions_result = gmaps.directions("GPS", "Lugar, Pais", mode="transit", departure_time=now)
 '''
+
+if __name__ == '__main__':
+    uvicorn.run(api_gpsd, host="0.0.0.0", port=8000)
